d2b282ab030c0054ee128d0967072e5710b79015
Elizaongwz <elizaongwangzheng@gmail.com>
2024-10-20 18:35:59 +0800

    main application springboot

---
+++ interface/src/main/java/com/dsa4264/BusModel/Project1Dsa4264Application.java
@@ -0,0 +1,13 @@
+package com.dsa4264.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Dsa4264Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Dsa4264Application.class, args);
+	}
+
+}
