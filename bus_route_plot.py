0f213d97d1143c0e6935181e6361933eb76e5df2
Wong Fang Ting <e0773420@u.nus.edu>
2024-11-02 17:21:16 +0800

    change ServiceNo to str (for frontend)

--- bus_route_plot.py
+++ bus_route_plot.py
@@ -47,7 +47,6 @@ def new_route(*service_numbers):
 
     singapore = get_mrt_map()
     new_data = pd.read_csv("new_top_10_bus_data.csv")
-    new_data['ServiceNo'] = new_data['ServiceNo'].astype(str)
 
     #if specific service numbers are provided, filter for them
     if service_numbers:
@@ -94,7 +93,6 @@ def original_and_new_route(*service_numbers):
 
     singapore = get_mrt_map()
     new_data = pd.read_csv("new_top_10_bus_data.csv")
-    new_data['ServiceNo'] = new_data['ServiceNo'].astype(str)
     
     if service_numbers:
         routes = new_data[new_data['ServiceNo'].isin(service_numbers)]
